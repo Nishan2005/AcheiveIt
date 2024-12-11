f540c5ebf92446cfd99ed89decf656a56a7dcfe2
reshdrai <141801808+reshdrai@users.noreply.github.com>
2024-12-11 15:54:18 +0545

    resh commit

---
+++ lib/constraints/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Color tdPrimary = Color(0xFFB39DCE);
+const Color tdText = Color(0xFF333333);
+const Color tdBlack = Color(0xFF454545);
+const Color tdBGColor = Color(0xFFF5F5F5);
+const Color tdGrey = Color(0xFFD0D0D0);
+const Color tdLessGrey = Color(0xFFE8E8E8);
